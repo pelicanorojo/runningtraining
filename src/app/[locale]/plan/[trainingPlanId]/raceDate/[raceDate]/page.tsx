6a4434d441527a9c09aeff0b0c2624cd52d37280
pelicanorojo <bioingbenito@gmail.com>
2025-07-08 22:02:06 -0300

    Fix on build detected bug. close #75

--- src/app/[locale]/plan/[trainingPlanId]/raceDate/[raceDate]/page.tsx
+++ src/app/[locale]/plan/[trainingPlanId]/raceDate/[raceDate]/page.tsx
@@ -2,7 +2,7 @@
  * @Author: Pablo Benito <pelicanorojo> bioingbenito@gmail.com
  * @Date: 2024-11-21T11:34:30-03:00
  * @Last modified by: Pablo Benito <pelicanorojo>
- * @Last modified time: 2025-05-21T10:25:42-03:00
+ * @Last modified time: 2025-07-08T09:59:49-03:00
  */
 
 
@@ -25,7 +25,6 @@ interface ShowPlanPageProps {
 
 export default async function ShowPlanPage({params}: ShowPlanPageProps) {
   const {trainingPlanId, raceDate, locale} = params;
-  const initialState = {trainingPlanId, raceDate};
   const today = formatDate(new Date());  
   //TODO: check trainingPlanId and raceDate are valid, if not redirect to 404.
   const rawPlanData = await loadRawPlanData(trainingPlanId , locale);
@@ -33,7 +32,7 @@ export default async function ShowPlanPage({params}: ShowPlanPageProps) {
   const defaultOrder = scheduledTrainings.find( t => t.trainingDate >= today)?.order || scheduledTrainings.length;
   return (
     <>
-    <ConfigBar trainingPlansAvailable={trainingPlansAvailableFront[locale]} initialState={initialState}/> 
+    <ConfigBar trainingPlansAvailable={trainingPlansAvailableFront[locale]}/> 
     <TrainingContainer  scheduledTrainings={scheduledTrainings} planDataParams={params} defaultOrder={defaultOrder}/>
     </>
   )
